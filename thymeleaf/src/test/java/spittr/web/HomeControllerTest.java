2d8ea9f9045aef4f15119fe03f3bfd17cb708f42
chenkangqiang <chenkangqiang@meituan.com>
2017-08-11 15:08:14 +0800

    reformat code

--- thymeleaf/src/test/java/spittr/web/HomeControllerTest.java
+++ thymeleaf/src/test/java/spittr/web/HomeControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 
-import spittr.web.HomeController;
 
 public class HomeControllerTest {
 
@@ -15,8 +14,7 @@ public class HomeControllerTest {
     public void testHomePage() throws Exception {
         HomeController controller = new HomeController();
         MockMvc mockMvc = standaloneSetup(controller).build();
-        mockMvc.perform(get("/"))
-                .andExpect(view().name("home"));
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
     }
 
 }
--- thymeleaf/src/test/java/spittr/web/SpittleControllerTest.java
+++ thymeleaf/src/test/java/spittr/web/SpittleControllerTest.java
@@ -1,6 +1,6 @@
 package spittr.web;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
