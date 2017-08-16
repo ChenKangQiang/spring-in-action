50ed86113468e7b9a474ed632e437c760619c196
chenkangqiang <chenkangqiang@meituan.com>
2017-08-16 14:32:02 +0800

    reformat code

--- thymeleaf/src/test/java/spittr/web/SpitterControllerTest.java
+++ thymeleaf/src/test/java/spittr/web/SpitterControllerTest.java
@@ -1,16 +1,14 @@
 package spittr.web;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import spittr.Spitter;
 import spittr.data.SpitterRepository;
-import spittr.web.SpitterController;
 
 public class SpitterControllerTest {
 
@@ -18,9 +16,9 @@ public class SpitterControllerTest {
     public void shouldShowRegistration() throws Exception {
         SpitterRepository mockRepository = mock(SpitterRepository.class);
         SpitterController controller = new SpitterController(mockRepository);
-        MockMvc mockMvc = standaloneSetup(controller).build();
-        mockMvc.perform(get("/spitter/register"))
-                .andExpect(view().name("registerForm"));
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/spitter/register"))
+                .andExpect(MockMvcResultMatchers.view().name("registerForm"));
     }
 
     @Test
@@ -31,15 +29,15 @@ public class SpitterControllerTest {
         when(mockRepository.save(unsaved)).thenReturn(saved);
 
         SpitterController controller = new SpitterController(mockRepository);
-        MockMvc mockMvc = standaloneSetup(controller).build();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
-        mockMvc.perform(post("/spitter/register")
+        mockMvc.perform(MockMvcRequestBuilders.post("/spitter/register")
                 .param("firstName", "Jack")
                 .param("lastName", "Bauer")
                 .param("username", "jbauer")
                 .param("password", "24hours")
                 .param("email", "jbauer@ctu.gov"))
-                .andExpect(redirectedUrl("/spitter/jbauer"));
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/spitter/jbauer"));
 
         verify(mockRepository, atLeastOnce()).save(unsaved);
     }
@@ -48,13 +46,13 @@ public class SpitterControllerTest {
     public void shouldFailValidationWithNoData() throws Exception {
         SpitterRepository mockRepository = mock(SpitterRepository.class);
         SpitterController controller = new SpitterController(mockRepository);
-        MockMvc mockMvc = standaloneSetup(controller).build();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
-        mockMvc.perform(post("/spitter/register"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("registerForm"))
-                .andExpect(model().errorCount(5))
-                .andExpect(model().attributeHasFieldErrors(
+        mockMvc.perform(MockMvcRequestBuilders.post("/spitter/register"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("registerForm"))
+                .andExpect(MockMvcResultMatchers.model().errorCount(5))
+                .andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors(
                         "spitter", "firstName", "lastName", "username", "password", "email"));
     }
 
